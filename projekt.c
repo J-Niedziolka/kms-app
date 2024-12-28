4f52b3f90c9d73336096df80aec34b212174fab7
Jan Niedziolka <j.niedziolka@samsung.com>
2024-12-28 22:31:18 +0100

    some changes

--- projekt.c
+++ projekt.c
@@ -23,22 +23,61 @@ struct pipeline_dev {
 	drmModeCrtc *saved_crtc;
 };
 
-static struct modeset_dev *modeset_list = 0;
+static struct pipeline_dev *modeset_devices = 0;
 
-void drmGatherConnectors(){
-	drmModeConne
+void drmGatherFiledes() {
+	printf("todo handle it properly\n");
 }
+
+void drmGatherConnectors(int drm_fd, drmModeRes * resources){
+	drmModeConnector *connector = NULL;
+	uint32_t temp_connectors[resources->count_connectors];
+	int conns = 0;
+	for (int i = 0; i < resources->count_connectors; i++) {
+		connector = drmModeGetConnector(drm_fd, resources->connectors[i]);
+		if (connector->connection == DRM_MODE_CONNECTED) {
+			printf("handled connector: %u\n", connector->connector_id);
+			temp_connectors[i] = connector->connector_id;
+			//break;
+			drmModeFreeConnector(connector);
+			conns++;
+		}
+		else {
+			printf("unhandled connector: %u\n", connector->connector_id);
+			drmModeFreeConnector(connector);
+			connector = NULL;
+		}
+	}
+
+	uint32_t handled_connectors[conns];
+	for(int i = 0; i < conns; i++){
+		handled_connectors[i] = temp_connectors[i];
+	}
+	printf("wychodzimy z drmgatherconnectors\n");
+}
+
 void userChooseConnector(struct pipeline_dev user_dev){
 	uint32_t connId = 0;
+	uint32_t * connPtr = &connId;
 	do{
-		scanf("Choose connector to be used: %d", &connId);
-	} while (connId ==NULL);
+		scanf("Choose connector to be used: %u", &connId);
+	} while (connPtr==NULL);
 
 	user_dev.connector = connId;
 }
 
 int main(int argc, char *argv[]) {
-	int drm_fd = open("/dev/dri/card1", O_RDWR | O_CLOEXEC);
+	int drm_fd = -1;
+	if (argc < 2){ //todo handle it properly
+		drmGatherFiledes();
+		printf("testing if we fall into contidtion\n");
+		return 0;
+	}
+	else {
+		//util_open(argv[1]);
+		//drmOpen(
+		drm_fd = open(argv[1], O_RDWR | O_CLOEXEC);
+	}
 	if (drm_fd < 0) {
 		perror("Can't open DRM device");
 		return -1;
@@ -51,16 +90,19 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
+	drmGatherConnectors(drm_fd, resources);
+	//printf("konektor: %u\n", &handled_connectors[0]);
+
 	//gather connectors
 	drmModeConnector *connector = NULL;
 	for (int i = 0; i < resources->count_connectors; i++) {
 		connector = drmModeGetConnector(drm_fd, resources->connectors[i]);
 		if (connector->connection == DRM_MODE_CONNECTED) {
-			printf("handled connector: %lu\n", connector->connector_id);
+			printf("handled connector: %u\n", connector->connector_id);
 			//break;
 		}
 		else {
-			printf("unhandled connector: %lu\n", connector->connector_id);
+			printf("unhandled connector: %u\n", connector->connector_id);
 			drmModeFreeConnector(connector);
 			connector = NULL;
 		}
